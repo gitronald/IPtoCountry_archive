1ae6d56ab7b011234384e9e92a89e92e5f5ef7be
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-26 23:40:59 -0800

    Add function documentation

--- R/IP_country.R
+++ R/IP_country.R
@@ -1,7 +1,18 @@
+#' Convert IP addresses to country names
+#'
+#' @param IPaddress A vector of IP addresses
+#'
+#' @return Returns a vector of country names for the locations of \code{IPaddress}
+#'
+#' @export
+#'
+#' @examples
+#' IP_country(IP_generator(1000))
+#'
+#'
 IP_country <- function(IPaddress) {
   # Convert IP address(es) to country(ies)
   #
-  require(data.table)
   IPcode <- IP_code(IPaddress)
   Country <- IP_lookup(IPdatabase, IPcode)
   Country <- factor(Country)
@@ -21,6 +32,18 @@ IP_code <- function(IPaddress) {
 }
 
 
+#' Title
+#'
+#' @param x IP database
+#' @param y IP addresses
+#'
+#' @return Returns country from IP codes
+#' @importFrom data.table setDT setkey data.table foverlaps
+#' @export
+#'
+#' @examples
+#' data1 <- IP_code(ips)
+#' IP_lookup(data1)
 IP_lookup <- function(x, y) {
   # Convert IP code(s) to country(ies)
   #
