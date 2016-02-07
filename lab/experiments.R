79482d80ce642cf753938e7e454f06a3c8d30310
dethfromabove <mejiajon@gmail.com>
2016-02-07 01:09:54 -0800

    Edited DecimalBinary.R

--- lab/experiments.R
+++ lab/experiments.R
@@ -1,25 +1,20 @@
-# Takes each component of decimals vector and converts it to raw bits vector.
-# This vector is then converted to an integer vector 8 bits long.
+# defines an IP address
 IP <- c(75,80,55,181)
-m <- sapply(IP, function(x) {
+
+# Takes each decimal value of the IP address provided and converts it to an
+# 8 bit binary string. The strings are then concatenated in IP_binary_string
+IP_binary_string <- c(IP_dataframe <- sapply(IP, function(x) {
   binary_vector <- rev(as.numeric(intToBits(x)))
   return(binary_vector[-(1:(length(binary_vector) - 8))])
 }
-)
-
-# Takes 4 8 bit vectors and combines them into 1 32 bit vector
-n <- c(m[,1:4])
-
-#
-prompt <- "enter the experiment numbers (space-separated list) \n"
-EXP <- as.integer(strsplit(readline(prompt), " ")[[1]])
+))
 
 
 # New methods -------------------------------------------------------------
 
 # Rough start/format example:
-x <- 75
-IP_binary <- function(x) {
+IP <- c(75)
+IP_binary <- sapply(IP, function(x)) {
   binary_vector <- rev(as.numeric(intToBits(x)))
   binary_vector <- binary_vector[-(1:(length(binary_vector) - 8))]
   return(binary_vector)
