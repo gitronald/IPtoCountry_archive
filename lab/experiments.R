fdc2391cd60b7a11e805744698cbced9a92520be
Ronald Robertson <gitronald@users.noreply.github.com>
2016-02-06 17:09:52 -0800

    Add details and corrections for developing binary converter

--- lab/experiments.R
+++ lab/experiments.R
@@ -15,10 +15,35 @@ prompt <- "enter the experiment numbers (space-separated list) \n"
 EXP <- as.integer(strsplit(readline(prompt), " ")[[1]])
 
 
+# New methods -------------------------------------------------------------
 
+# Rough start/format example:
 x <- 75
 IP_binary <- function(x) {
   binary_vector <- rev(as.numeric(intToBits(x)))
   binary_vector <- binary_vector[-(1:(length(binary_vector) - 8))]
   return(binary_vector)
 }
+
+### Example
+
+# Create a single sample IP
+x <- IP_generator(1)
+
+# Create method for splitting an IP address values into matrix (pulled from IP_integer):
+IP_split <- function(IP.address) {
+  IPa <- as.character(IP.address)  # Convert value(s) to character
+  IPs <- strsplit(IPa, "\\.")      # Split on "." ("\\" used for special characters)
+  IPs <- lapply(IPs, as.numeric)   # Convert list(s) to numeric
+  IPs <- do.call(rbind, IPs)       # Rbind lists to matrix
+  return(IPs)                      # Return IP data.frame
+}
+
+# Test on sample
+y <- IP_split(x)
+
+# Proof of concept for IP_binary
+lapply(y, function(x) as.numeric(intToBits(x))) # E.g. For each column of y, convert value to numeric binary
+
+
+
