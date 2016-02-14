d214fd7f81fada4e2bbdd6efe2b7c439e6a8e8e6
dethfromabove <mejiajon@gmail.com>
2016-02-14 14:16:07 -0800

    Rename IP_split to IP_binary

--- R/IP_binary.R
+++ R/IP_binary.R
@@ -2,14 +2,15 @@
 x <- IP_generator(1)
 
 # Create method for splitting an IP address values into matrix (pulled from IP_integer):
-IP_split <- function(IP.address) {
+IP_binary <- function(IP.address) {
   IPa <- as.character(IP.address)  # Convert value(s) to character
   IPs <- strsplit(IPa, "\\.")      # Split on "." ("\\" used for special characters)
   IPs <- lapply(IPs, function(x) {
     binary_vector <- rev(as.numeric(intToBits(x)))
     binary_vector <- binary_vector[-(1:(length(binary_vector) - 8))]
     return(binary_vector)
-  })   # Convert list(s) to numeric
+    })                             # Reverses order of numeric bits and 
+                                   # removes 24 leading bits
   IPs <- do.call(rbind, IPs)       # Rbind lists to matrix
   return(IPs)                      # Return IP data.frame
 }
